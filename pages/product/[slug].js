7390dbfc9fcef7c2a60c5ead3ea7803b16b13767
Isaac <isaac231467@gmail.com>
2022-11-24 17:57:15 -0600

    fixed up the slug page still need to add a slider for the feature products

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,11 +1,13 @@
 import React from 'react'
+import SneakersProductCards from '../../Components/SneakersProductCards';
 import { client, urlFor } from '../../lib/client'
 
-const ProductDetail = ({product}) => {
+const ProductDetail = ({product, products}) => {
   const {name, details, price } = product;
   const imageOfProduct = product.image[0].asset._ref;
   return (
     <div>
+    <div className='main-product-wrapper'>
         <div className='product-detail-container'>
                 <div className='image-container'>
                   <img src={urlFor(imageOfProduct)} className='product-detail-image'/>
@@ -13,10 +15,28 @@ const ProductDetail = ({product}) => {
                 <div className='detail-specs'>
                   <h2 id='details'>{name}</h2>
                   <p id='details'>${price}</p>
-                  <p id='details'>{details}</p>
+                  <div className='details-container'>
+                    <p id='details'>{details}</p>
+                  </div>
+                  <div className='button-container'>
+                    <button type="button" className='buy-now'>Buy Now!</button>
+                  </div>
                 </div>
         </div>
     </div>
+
+    <div className='featured-title-container'>
+          <h3>Products You May Also Like</h3>
+    </div>
+    <div className='featured-products'>
+        
+        <div className='products-featured'>
+          {
+            products?.map((items) => <SneakersProductCards  key={items._id} products={items} />)
+          }
+        </div>
+    </div>
+  </div>
   )
 }
 
@@ -46,8 +66,12 @@ return {
 export const getStaticProps = async ({params: {slug}}) => {
   const query = `*[_type == "sneakers" && slug.current == '${slug}'][0]`;
   const product = await client.fetch(query);
+
+  const queryProducts = '*[_type =="sneakers"]';
+  const products = await client.fetch(queryProducts);
+
   return {
-    props: {product}
+    props: { product , products }
   }
 };
 
