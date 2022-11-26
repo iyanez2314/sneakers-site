06f41b26d75530a57a97b9726df73a829ad07ccd
Isaac <isaac231467@gmail.com>
2022-11-26 10:05:31 -0600

    I think I will come back to this due to the fact I will have to add photos to the files and or do a fetch for a product and I really do not want to do that, so I will get started on the functionnality of the page with state management and then I will do the cart once I have the state management down.

--- Components/Cart.jsx
+++ Components/Cart.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Cart = () => {
+  // everything in this component will be rendered dynamicly 
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper'>
+    <div className='cart-container'>
+      <h3 className='heading'>Your cart</h3>
+      <span className='cart-num-items'>total items</span>
+      <div className='product-container'>
+        {/*  Here we will put products that have been added to cart but this will be dynamiclly rendered */}
+      </div>
+    </div>
+    </div>
   )
 }
 
